5bfba6cd0bd596dba0a90f2592031f561ccc51b5
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2026-08-30 03:32:52 +0100

    removed python

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ mkdir -p "$tmp_path"
 curl -L https://github.com/TaiseiYokoshima/install/archive/refs/heads/main.tar.gz | tar -xz --strip-components=1 -C "$tmp_path"
 
 cd "$tmp_path"
-nix-shell -p git gh python3 python --run "./install"
+nix-shell -p git gh python3 --run "./install"
